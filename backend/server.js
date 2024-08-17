d0233570d718f11d0e75247ec2b6641707728a4b
Pasindu Wanigaratne <pasinda4@gmail.com>
2024-08-17 21:58:58 +0530

    1st

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const express = require ('express')
+const mongoose = require ('mongoose')
+
+//express app
+const app = express()
+const workoutRoutes = require('./routes/workouts')
+
+//middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+//routes
+app.use('/api/workouts',workoutRoutes)
+
+
+//connect to mongodb
+mongoose.connect(process.env.MONG_URI)
+   .then(() => {
+       console.log('connected to mongodb')
+       //listen for requests
+       app.listen(process.env.PORT, () => {
+       console.log('Connected to DB and listening on port', process.env.PORT)
+})
+   })
+    .catch((err) => {
+         console.log(err)
+    })
+
+
+
+
